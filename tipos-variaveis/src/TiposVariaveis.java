7b51b2372e42415d7e54690989affd2b46157370
gleisonsensui <gleison.nsilva@hotmail.com>
2024-08-05 21:55:54 -0300

    Leaning types and variables.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,18 @@
 public class TiposVariaveis {
     
+    public static void main(String[] args) {
+        // double salarioMinimo = 1418.00;
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        // final double VALOR_DE_PI = 3.141516;
+
+        System.out.println(numero);
+
+    }
 }
